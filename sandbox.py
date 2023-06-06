b4fe05e98f8c8cd6a4171bacc14a5a61431bd44d
Rohan Bhatt <rohanbhattcs@gmail.com>
2023-06-06 12:31:14 -0400

    Update sandbox.py

--- sandbox.py
+++ sandbox.py
@@ -1,9 +1,30 @@
-def is_present(lst,x):
-  lst2 = [y for y in lst if y == x]
-  print(list(lst2))
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+    return arr
+
+def insertionSort(arr):
+    if len(arr) < 2:
+      return arr
+    for i in range(1, len(arr)):
+        key = arr[i]
+
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+
 
 def main():
-    lst = [1,2,3,4,5,6,7,8,9,10]
-    is_present(lst, 4)
+    arr = [2,1,3,5,6]
+    insertionSort(arr)
+    print(arr)
+
 if __name__ == "__main__":
     main()
